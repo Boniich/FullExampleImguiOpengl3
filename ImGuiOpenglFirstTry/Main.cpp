223e55003ac8821c9752b645348e47129e67bce9
Boniich <ezequieldbo25@gmail.com>
2024-03-08 12:11:34 -0300

    feat: add files to work docking branch

--- ImGuiOpenglFirstTry/Main.cpp
+++ ImGuiOpenglFirstTry/Main.cpp
@@ -1,53 +1,3 @@
-//#include <iostream>
-//#include <glad/glad.h>
-//#include <GLFW/glfw3.h>
-//#include <imgui/imgui.h>
-//
-//int main()
-//{
-//
-//    glfwInit();
-//    ImGui::CreateContext();
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-//
-//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//
-//    GLFWwindow* window = glfwCreateWindow(800, 800, "My window", NULL, NULL);
-//
-//    if (window == NULL) {
-//        std::cout << "Failed to create window";
-//        glfwTerminate();
-//        return -1;
-//    }
-//
-//    glfwMakeContextCurrent(window);
-//
-//    gladLoadGL();
-//
-//    //glViewport(0, 0, 800, 800);
-//
-//    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-//    glClear(GL_COLOR_BUFFER_BIT);
-//    glfwSwapBuffers(window);
-//
-//    while (!glfwWindowShouldClose(window))
-//    {
-//        glfwPollEvents();
-//        ImGui::NewFrame();
-//    }
-//
-//    ImGui::EndFrame();
-//    ImGui::DestroyContext();
-//
-//    glfwDestroyWindow(window);
-//    glfwTerminate();
-//
-//    return 0;
-//
-//}
-
-
 // Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
 // (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
 
@@ -57,7 +7,7 @@
 // - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
 // - Introduction, links and more at the top of imgui.cpp
 
-#include <imgui/imgui.h>
+#include "imgui/imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
@@ -127,10 +77,22 @@ int main(int, char**)
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+    //io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+    //io.ConfigViewportsNoAutoMerge = true;
+    //io.ConfigViewportsNoTaskBarIcon = true;
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
     //ImGui::StyleColorsLight();
+
+    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
 
     // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOpenGL(window, true);
@@ -228,6 +190,17 @@ int main(int, char**)
         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // Update and Render additional Platform Windows
+        // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+        //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            GLFWwindow* backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
 
         glfwSwapBuffers(window);
     }
